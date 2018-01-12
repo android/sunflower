52b0c5e288e39fd80af51cac9a74f77da0689278
Tiem Song <tiem@google.com>
2018-01-11 23:21:53 -0800

    Android Sunflower: add function to calculate grow zone.
    Code reviewers: added a function to map a given latitude to a Plant's
     Grow Zone. This will be used when adding Location to allow the user
     to filter the list of plans by their location's grow zone.
    
    Tracker: go/android-sunflower-tracker
    PRD: go/android-sunflower-prd
    
    Bug: 65952046
    Test: ran unit and instrumented tests:
    ./gradlew app:test app:connectedAndroidTest
    
    Change-Id: Ib64827453c6f6ae957f4aed4f4c5964dcef47aaf

---
+++ app/src/test/java/com/google/samples/apps/sunflower/utilities/GrowZoneUtilTest.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower.utilities
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GrowZoneUtilTest {
+
+    @Test fun getZoneForLatitude() {
+        assertEquals(13, getZoneForLatitude(0.0))
+        assertEquals(13, getZoneForLatitude(7.0))
+        assertEquals(12, getZoneForLatitude(7.1))
+        assertEquals(1, getZoneForLatitude(84.1))
+        assertEquals(1, getZoneForLatitude(90.0))
+    }
+
+    @Test fun getZoneForLatitude_negativeLatitudes() {
+        assertEquals(13, getZoneForLatitude(-7.0))
+        assertEquals(12, getZoneForLatitude(-7.1))
+        assertEquals(1, getZoneForLatitude(-84.1))
+        assertEquals(1, getZoneForLatitude(-90.0))
+    }
+}
