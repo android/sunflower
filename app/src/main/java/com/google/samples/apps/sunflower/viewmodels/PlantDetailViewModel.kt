69454bece8dcd66d98698da4e4ace9ad76f28ad0
Tiem Song <tiem@google.com>
2018-04-19 22:28:14 -0700

    Android Sunflower: convert ViewModel + Factory to Kotlin
    This CL is part of the effort in moving project entirely to Kotlin.
    
    Bug: 74392384
    Test: ran unit and instrumented tests:
          ./gradlew app:test app:connectedAndroidTest
    
    Change-Id: Ie1366a24dc03ce24ac2658398629cb6a405a9c84

---
+++ app/src/main/java/com/google/samples/apps/sunflower/viewmodels/PlantDetailViewModel.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower.viewmodels
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Transformations
+import android.arch.lifecycle.ViewModel
+import com.google.samples.apps.sunflower.PlantDetailActivity
+import com.google.samples.apps.sunflower.PlantDetailFragment
+import com.google.samples.apps.sunflower.data.GardenPlantingRepository
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.data.PlantRepository
+
+/**
+ * The ViewModel used in [PlantDetailActivity] and [PlantDetailFragment].
+ */
+class PlantDetailViewModel(
+        plantRepository: PlantRepository,
+        private val gardenPlantingRepository: GardenPlantingRepository,
+        private val plantId: String
+) : ViewModel() {
+
+    val isPlanted: LiveData<Boolean>
+    val plant: LiveData<Plant>
+
+    init {
+
+        /* The getGardenPlantingForPlant method returns a LiveData from querying the database. The
+         * method can return null in two cases: when the database query is running and if no records
+         * are found. In these cases isPlanted is false. If a record is found then isPlanted is
+         * true. */
+        val gardenPlantingForPlant = gardenPlantingRepository.getGardenPlantingForPlant(plantId)
+        isPlanted = Transformations.map(gardenPlantingForPlant) { it != null }
+
+        plant = plantRepository.getPlant(plantId)
+    }
+
+    fun addPlantToGarden() {
+        gardenPlantingRepository.createGardenPlanting(plantId)
+    }
+}
