1f865af28ffd146731ec3094ed0abbe0f6cb1a7b
Tiem Song <tiem@google.com>
2018-04-25 21:14:53 -0700

    Android Sunflower: modify plant list item layout
    - Add image of plant
    - Remove plant ID
    
    Test: ran unit and instrumented tests:
          ./gradlew app:test app:connectedAndroidTest
    
    Change-Id: Ibf55834c5c9c6fcbd16376ff7a250faeb4304ba1

--- app/src/main/java/com/google/samples/apps/sunflower/adapters/PlantAdapter.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/adapters/PlantAdapter.kt
@@ -21,7 +21,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.google.samples.apps.sunflower.PlantDetailActivity
 import com.google.samples.apps.sunflower.PlantDetailFragment
 import com.google.samples.apps.sunflower.PlantListActivity
@@ -57,14 +60,16 @@ class PlantAdapter(
             }
             view.context.startActivity(intent)
         }
-
     }
 
     override fun getItemCount() = values.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.apply {
-            idView.text = values[position].plantId
+            Glide.with(imageView.context)
+                    .load(values[position].imageUrl)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(imageView)
             contentView.text = values[position].name
             with(itemView) {
                 tag = values[position]
@@ -84,7 +89,7 @@ class PlantAdapter(
      * @param itemView - view to store in the ViewHolder
      */
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val idView: TextView = itemView.findViewById(R.id.id_text)
-        val contentView: TextView = itemView.findViewById(R.id.content)
+        val imageView: ImageView = itemView.findViewById(R.id.plant_item_image)
+        val contentView: TextView = itemView.findViewById(R.id.plant_item_title)
     }
 }
