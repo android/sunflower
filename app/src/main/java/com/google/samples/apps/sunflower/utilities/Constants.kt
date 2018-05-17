2af6cd1e515ce3acb222a0ad02355c06088f4c85
Tiem Song <tiem@google.com>
2018-05-17 15:29:00 -0700

    [Sunflower] Simplfy JSON parsing
    Test: ran unit and instrumented tests:
          ./gradlew app:test app:connectedAndroidTest
    
    Change-Id: I428898ff0d4cf0d5aef8c41b06e9213541407935

--- app/src/main/java/com/google/samples/apps/sunflower/utilities/Constants.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/utilities/Constants.kt
@@ -20,3 +20,4 @@ package com.google.samples.apps.sunflower.utilities
  * Constants used throughout the app.
  */
 const val DATABASE_NAME = "sunflower-db"
+const val PLANT_DATA_FILENAME = "plants.json"
--- app/src/main/java/com/google/samples/apps/sunflower/workers/SeedDatabaseWorker.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/workers/SeedDatabaseWorker.kt
@@ -20,24 +20,30 @@ import android.util.Log
 import androidx.work.Worker
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
 import com.google.samples.apps.sunflower.data.AppDatabase
 import com.google.samples.apps.sunflower.data.Plant
-import com.google.samples.apps.sunflower.utilities.readJson
+import com.google.samples.apps.sunflower.utilities.PLANT_DATA_FILENAME
 
 class SeedDatabaseWorker : Worker() {
     private val TAG = SeedDatabaseWorker::class.java.simpleName
 
     override fun doWork(): WorkerResult {
         val plantType = object : TypeToken<List<Plant>>() {}.type
+        var jsonReader: JsonReader? = null
 
         return try {
-            val plantList: List<Plant> = Gson().fromJson(readJson(applicationContext), plantType)
+            val inputStream = applicationContext.assets.open(PLANT_DATA_FILENAME)
+            jsonReader = JsonReader(inputStream.reader())
+            val plantList: List<Plant> = Gson().fromJson(jsonReader, plantType)
             val database = AppDatabase.getInstance(applicationContext)
             database.plantDao().insertAll(plantList)
             WorkerResult.SUCCESS
         } catch (ex: Exception) {
             Log.e(TAG, "Error seeding database", ex)
             WorkerResult.FAILURE
+        } finally {
+            jsonReader?.close()
         }
     }
 }
