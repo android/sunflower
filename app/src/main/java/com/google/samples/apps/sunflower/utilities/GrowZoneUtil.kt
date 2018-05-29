47bcd8f112694fb45a1f6f349213e509f26c07de
Tiem Song <tiem@google.com>
2018-05-29 14:15:08 -0700

    [Sunflower] Fix latitude zones
    Fixes https://github.com/googlesamples/android-sunflower/issues/8
    
    Change-Id: I80f25b67fc10795d39b7a84f7f9093b0733fa66b

--- app/src/main/java/com/google/samples/apps/sunflower/utilities/GrowZoneUtil.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/utilities/GrowZoneUtil.kt
@@ -35,12 +35,12 @@ fun getZoneForLatitude(latitude: Double) = when (Math.abs(latitude)) {
     in 7.0..14.0 -> 12
     in 14.0..21.0 -> 11
     in 21.0..28.0 -> 10
-    in 28.0..21.0 -> 9
-    in 35.0..21.0 -> 8
-    in 42.0..21.0 -> 7
-    in 49.0..21.0 -> 6
-    in 56.0..21.0 -> 5
-    in 63.0..21.0 -> 4
+    in 28.0..35.0 -> 9
+    in 35.0..42.0 -> 8
+    in 42.0..49.0 -> 7
+    in 49.0..56.0 -> 6
+    in 56.0..63.0 -> 5
+    in 63.0..70.0 -> 4
     in 70.0..77.0 -> 3
     in 77.0..84.0 -> 2
     else -> 1 // Remaining latitudes are assigned to zone 1.
