3b1c25c4a247a02f6414fc2583a2014fea544139
XinyueZ <xinyue.zhao@immonet.de>
2018-06-02 22:30:55 +0200

    A solution to fixed issue #10
    - This solution has saved state of recycler-view and restore it after
      plan-list would have been loaded.
    - There're also other ways to fix it, here might be comfortable for me.

--- app/src/main/java/com/google/samples/apps/sunflower/PlantListFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/PlantListFragment.kt
@@ -19,6 +19,7 @@ package com.google.samples.apps.sunflower
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.os.Parcelable
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
@@ -35,25 +36,40 @@ class PlantListFragment : Fragment() {
 
     private lateinit var viewModel: PlantListViewModel
     private var arePlantsFiltered = false // TODO remove this, used for development
+    private lateinit var binding: FragmentPlantListBinding
+    private val BUNDLE_RECYCLER_LAST_STATE by lazy { "recycler.last.state" }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = FragmentPlantListBinding.inflate(inflater, container, false).run {
-        setLifecycleOwner(this@PlantListFragment)
         val context = context ?: return root
+        setLifecycleOwner(this@PlantListFragment)
+        binding = this
+
         val factory = InjectorUtils.providePlantListViewModelFactory(context)
         viewModel = ViewModelProviders.of(this@PlantListFragment, factory)
             .get(PlantListViewModel::class.java)
 
         with(PlantAdapter()) {
             plantList.adapter = this
-            subscribeUi(this@run, this)
+            subscribeUi(binding, this, getRecyclerViewState(savedInstanceState))
         }
 
         setHasOptionsMenu(true)
         root
+    }
+
+    private fun getRecyclerViewState(savedInstanceState: Bundle?): Parcelable? =
+        savedInstanceState?.getParcelable(BUNDLE_RECYCLER_LAST_STATE)
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(
+            BUNDLE_RECYCLER_LAST_STATE,
+            binding.plantList.layoutManager.onSaveInstanceState()
+        )
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
@@ -70,11 +86,19 @@ class PlantListFragment : Fragment() {
         }
     }
 
-    private fun subscribeUi(databinding: FragmentPlantListBinding, adapter: PlantAdapter) {
+    private fun subscribeUi(
+        databinding: FragmentPlantListBinding,
+        adapter: PlantAdapter,
+        recyclerViewState: Parcelable?
+    ) {
         viewModel.getPlants().observe(this, Observer { plants ->
             if (plants != null) {
                 adapter.values = plants
                 databinding.loadingUi.visibility = GONE
+
+                recyclerViewState?.apply {
+                    databinding.plantList.layoutManager.onRestoreInstanceState(recyclerViewState)
+                }
             }
         })
     }
