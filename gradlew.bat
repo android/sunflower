5218be78bb857644853645cc3f639dd772192850
ksnraju839 <ksnraju839@gmail.com>
2018-08-21 16:03:25 +0530

    change

--- gradlew.bat
+++ gradlew.bat
@@ -1,7 +1,7 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
-@rem  Gradle startup script for Windows
+@rem  Gradle startup script for Windowsssss
 @rem
 @rem ##########################################################################
 
