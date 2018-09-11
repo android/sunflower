67c22d74196f7e9a0021abc4895036d7a709497f
William Wu <williamwue@gmail.com>
2018-09-11 17:39:10 +0800

    feat: add test for showSnackbarMessage

--- app/src/androidTest/java/com/google/samples/apps/sunflower/viewmodels/PlantDetailViewModelTest.kt
+++ app/src/androidTest/java/com/google/samples/apps/sunflower/viewmodels/PlantDetailViewModelTest.kt
@@ -18,13 +18,17 @@ package com.google.samples.apps.sunflower.viewmodels
 
 import android.arch.persistence.room.Room
 import android.support.test.InstrumentationRegistry
+import android.support.test.annotation.UiThreadTest
+import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.AppDatabase
 import com.google.samples.apps.sunflower.data.GardenPlantingRepository
 import com.google.samples.apps.sunflower.data.PlantRepository
 import com.google.samples.apps.sunflower.utilities.getValue
 import com.google.samples.apps.sunflower.utilities.testPlant
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -53,5 +57,13 @@ class PlantDetailViewModelTest {
     @Throws(InterruptedException::class)
     fun testDefaultValues() {
         assertFalse(getValue(viewModel.isPlanted))
+    }
+
+    @Test
+    @UiThreadTest
+    fun testSnackbarMessage_AddedPlantToGardenString() {
+        assertNotEquals(viewModel.snackbarText.value, R.string.added_plant_to_garden)
+        viewModel.showSnackbarMessage(R.string.added_plant_to_garden)
+        assertEquals(viewModel.snackbarText.value, R.string.added_plant_to_garden)
     }
 }
