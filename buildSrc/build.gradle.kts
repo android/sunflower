4b1a36ebd231358135b5edba42a05580e63e3968
Jean-Michel Fayard <jmfayard@gmail.com>
2018-10-08 10:53:02 +0200

    Refactor some build logic to the buildSrc & gradle kotlin-dsl
    Files buildSrc/src/main/java/{Versions.kt,Libs.kt} are generated with the task
       $ ./gradlew syncLibs
    
    Signed-off-by: Jean-Michel Fayard <jmfayard@gmail.com>

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,8 @@
+
+plugins {
+    `kotlin-dsl`
+}
+repositories {
+    jcenter()
+}
+        
---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,6 @@
+object Config {
+    const val compileSdkVersion = 28
+    const val minSdkVersion = 19
+    const val targetSdkVersion = 28
+    const val ktlintVersion = "0.28.0"
+}
