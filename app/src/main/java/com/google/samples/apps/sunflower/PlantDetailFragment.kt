d529c795f17f9c0a4beb7290ab6065aa66c98e2f
Tuan Nguyen <nttuan2411@gmail.com>
2018-10-10 14:00:40 +0700

    refactor imports

--- app/src/main/java/com/google/samples/apps/sunflower/PlantDetailFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/PlantDetailFragment.kt
@@ -19,7 +19,12 @@ package com.google.samples.apps.sunflower
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
