dc521f513f89646d31b9b8c339c9b0ac1fa729d5
Pietro F. Maggi <pmaggi@google.com>
2019-02-22 15:11:04 +0000

    Updated WorkManager to 1.0.0-rc02 and refactored SeedDatabaseWorker (#345)
    * Updated WorkManager to 1.0.0-rc02 and refactored SeedDatabaseWorker
    
    SeedDatabaseWorker is now a CoroutineWorker
    
    * Fixed identation.

--- app/src/main/java/com/google/samples/apps/sunflower/workers/SeedDatabaseWorker.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/workers/SeedDatabaseWorker.kt
@@ -18,7 +18,7 @@ package com.google.samples.apps.sunflower.workers
 
 import android.content.Context
 import android.util.Log
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -26,16 +26,20 @@ import com.google.gson.stream.JsonReader
 import com.google.samples.apps.sunflower.data.AppDatabase
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.utilities.PLANT_DATA_FILENAME
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
 
 class SeedDatabaseWorker(
     context: Context,
     workerParams: WorkerParameters
-) : Worker(context, workerParams) {
+) : CoroutineWorker(context, workerParams) {
 
     private val TAG by lazy { SeedDatabaseWorker::class.java.simpleName }
+    override val coroutineContext = Dispatchers.IO
 
-    override fun doWork(): Result {
-        return try {
+    override suspend fun doWork(): Result = coroutineScope {
+
+        try {
             applicationContext.assets.open(PLANT_DATA_FILENAME).use { inputStream ->
                 JsonReader(inputStream.reader()).use { jsonReader ->
                     val plantType = object : TypeToken<List<Plant>>() {}.type
