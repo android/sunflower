8164753279950ff97ae5ee861376c140c4ff2509
Nicole Borrelli <borrelli@google.com>
2019-02-22 13:17:17 -0800

    Add basic support for RecyclerView-selection
    Styling needs to be fixed up, and code added to enable/disable
    ActionMode.
    
    Change-Id: I55034ce9869453ca4e8f38d95f47381bff292662

--- app/src/main/java/com/google/samples/apps/sunflower/GardenFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/GardenFragment.kt
@@ -23,12 +23,25 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.selection.SelectionPredicates
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StableIdKeyProvider
+import androidx.recyclerview.selection.StorageStrategy
 import com.google.samples.apps.sunflower.adapters.GardenPlantingAdapter
+import com.google.samples.apps.sunflower.adapters.GardenPlantingDetailsLookup
 import com.google.samples.apps.sunflower.databinding.FragmentGardenBinding
 import com.google.samples.apps.sunflower.utilities.InjectorUtils
 import com.google.samples.apps.sunflower.viewmodels.GardenPlantingListViewModel
 
 class GardenFragment : Fragment() {
+
+    private lateinit var selectionTracker: SelectionTracker<Long>
+    private val selectionObserver = object : SelectionTracker.SelectionObserver<Long>() {
+        override fun onSelectionChanged() {
+            super.onSelectionChanged()
+            onSelectionChanged(selectionTracker.selection?.size() ?: 0)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,10 +55,26 @@ class GardenFragment : Fragment() {
         return binding.root
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        requireActivity().addOnBackPressedCallback {
+            onBackPressed()
+        }
+    }
+
+    private fun onBackPressed() =
+        if (selectionTracker.hasSelection()) {
+            selectionTracker.clearSelection()
+            true
+        } else {
+            false
+        }
+
     private fun subscribeUi(adapter: GardenPlantingAdapter, binding: FragmentGardenBinding) {
         val factory = InjectorUtils.provideGardenPlantingListViewModelFactory(requireContext())
         val viewModel = ViewModelProviders.of(this, factory)
-                .get(GardenPlantingListViewModel::class.java)
+            .get(GardenPlantingListViewModel::class.java)
 
         viewModel.gardenPlantings.observe(viewLifecycleOwner, Observer { plantings ->
             binding.hasPlantings = (plantings != null && plantings.isNotEmpty())
@@ -55,5 +84,22 @@ class GardenFragment : Fragment() {
             if (result != null && result.isNotEmpty())
                 adapter.submitList(result)
         })
+
+        val recyclerView = binding.gardenList
+        selectionTracker = SelectionTracker.Builder<Long>(
+            GardenFragment::javaClass.name,
+            recyclerView,
+            StableIdKeyProvider(recyclerView),
+            GardenPlantingDetailsLookup(recyclerView),
+            StorageStrategy.createLongStorage()
+        ).withSelectionPredicate(
+            SelectionPredicates.createSelectAnything()
+        ).build()
+        adapter.tracker = selectionTracker
+        selectionTracker.addObserver(selectionObserver)
+    }
+
+    private fun onSelectionChanged(selectedCount: Int) {
+        // TODO: Enable/disable ActionMode when items are selected.
     }
 }
