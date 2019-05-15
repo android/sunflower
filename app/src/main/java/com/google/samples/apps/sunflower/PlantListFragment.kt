030de19d4cf5c124834811731821219fba0952cd
madroidmaq <madroidmaq@gmail.com>
2019-05-16 01:49:43 +0800

    Initialize the ViewModel using the extension function (#406)
    Official documents can be viewed :
    https://developer.android.com/reference/kotlin/androidx/fragment/app/package-summary#viewmodels

--- app/src/main/java/com/google/samples/apps/sunflower/PlantListFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/PlantListFragment.kt
@@ -24,8 +24,8 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.google.samples.apps.sunflower.adapters.PlantAdapter
 import com.google.samples.apps.sunflower.databinding.FragmentPlantListBinding
 import com.google.samples.apps.sunflower.utilities.InjectorUtils
@@ -33,7 +33,9 @@ import com.google.samples.apps.sunflower.viewmodels.PlantListViewModel
 
 class PlantListFragment : Fragment() {
 
-    private lateinit var viewModel: PlantListViewModel
+    private val viewModel: PlantListViewModel by viewModels {
+        InjectorUtils.providePlantListViewModelFactory(requireContext())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,10 +43,7 @@ class PlantListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentPlantListBinding.inflate(inflater, container, false)
-        val context = context ?: return binding.root
-
-        val factory = InjectorUtils.providePlantListViewModelFactory(context)
-        viewModel = ViewModelProviders.of(this, factory).get(PlantListViewModel::class.java)
+        context ?: return binding.root
 
         val adapter = PlantAdapter()
         binding.plantList.adapter = adapter
