6c2cae99dba6c07474131ed13ca550781bdffe91
Tiem Song <tiem@google.com>
2019-06-17 14:37:14 -0700

    Add button to navigate to Plant List
    When the user's garden is empty, add a button prompting them to navigate to the Plant List.
    
    This also adds a new string and some placeholder string translations.
    
    Updated styling will be implemented in a future commit.

--- app/src/main/java/com/google/samples/apps/sunflower/GardenFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/GardenFragment.kt
@@ -23,12 +23,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
 import com.google.samples.apps.sunflower.adapters.GardenPlantingAdapter
 import com.google.samples.apps.sunflower.databinding.FragmentGardenBinding
 import com.google.samples.apps.sunflower.utilities.InjectorUtils
 import com.google.samples.apps.sunflower.viewmodels.GardenPlantingListViewModel
 
 class GardenFragment : Fragment() {
+
+    private lateinit var binding: FragmentGardenBinding
 
     private val viewModel: GardenPlantingListViewModel by viewModels {
         InjectorUtils.provideGardenPlantingListViewModelFactory(requireContext())
@@ -39,11 +42,20 @@ class GardenFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentGardenBinding.inflate(inflater, container, false)
+        binding = FragmentGardenBinding.inflate(inflater, container, false)
         val adapter = GardenPlantingAdapter()
         binding.gardenList.adapter = adapter
         subscribeUi(adapter, binding)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.addPlant.setOnClickListener {
+            val direction = GardenFragmentDirections.actionGardenFragmentToPlantListFragment()
+            findNavController().navigate(direction)
+        }
     }
 
     private fun subscribeUi(adapter: GardenPlantingAdapter, binding: FragmentGardenBinding) {
