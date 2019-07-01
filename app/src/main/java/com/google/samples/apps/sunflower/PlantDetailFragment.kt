238111846ca20a78e5e8860743e89e1fc0fd8a2c
Gautam Ajjarapu <gautam@ajjarapu.com>
2019-07-01 14:10:11 -0700

    removed unused imports

--- app/src/main/java/com/google/samples/apps/sunflower/PlantDetailFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/PlantDetailFragment.kt
@@ -20,9 +20,6 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.coordinatorlayout.widget.CoordinatorLayout
