b580dbcbd76f1ef0f49d074b1f416505151b2574
Gautam Ajjarapu <gautam@ajjarapu.com>
2019-07-02 11:45:57 -0700

    Detail Collapsing Toolbar (#470)
    * initial commit for detail menu items
    
    * spotlessApply
    
    * spotlessApply
    
    * removed unused methods
    
    * removed unused imports
    
    * navbar code review
    
    * fixed overlap

--- app/src/main/java/com/google/samples/apps/sunflower/PlantDetailFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/PlantDetailFragment.kt
@@ -20,9 +20,6 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.coordinatorlayout.widget.CoordinatorLayout
@@ -37,6 +34,8 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.samples.apps.sunflower.databinding.FragmentPlantDetailBinding
 import com.google.samples.apps.sunflower.utilities.InjectorUtils
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
+import androidx.navigation.findNavController
+import androidx.core.widget.NestedScrollView
 
 /**
  * A fragment representing a single Plant detail screen.
@@ -64,6 +63,49 @@ class PlantDetailFragment : Fragment() {
                 plantDetailViewModel.addPlantToGarden()
                 Snackbar.make(view, R.string.added_plant_to_garden, Snackbar.LENGTH_LONG).show()
             }
+
+            toolbarLayout.setCollapsedTitleTextColor(R.color.colorPrimaryDark)
+
+            // scroll change listener begins at Y = 0 when image is fully collapsed
+            plantDetailScrollview.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
+
+                // if scroll past image to height of toolbar, plant name in body is off screen
+                if (scrollY > toolbar.height) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+                        // convert dp value from dimens to pixel value
+                        // set regular shadow to toolbar
+                        appbar.elevation = resources.displayMetrics.density *
+                                resources.getDimension(R.dimen.toolbar_elevation)
+                    }
+
+                    // set toolbar title to plant name
+                    toolbarLayout.title = viewModel?.plant?.value?.name
+                } else { // if plant name in body is still visible
+
+                    // remove title (since plant name in body is used as title)
+                    toolbarLayout.title = " "
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+                        // remove shadow from toolbar
+                        appbar.elevation = 0.0f
+                    }
+                }
+            })
+
+            toolbar.setNavigationOnClickListener { view ->
+                view.findNavController().navigateUp()
+            }
+
+            toolbar.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.action_share -> {
+                        createShareIntent()
+                        true
+                    }
+                    else -> false
+                }
+            }
         }
 
         plantDetailViewModel.plant.observe(this) { plant ->
@@ -79,34 +121,25 @@ class PlantDetailFragment : Fragment() {
         return binding.root
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_plant_detail, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
+    // Helper function for calling a share functionality.
+    // Should be used when user presses a share button/menu item.
     @Suppress("DEPRECATION")
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_share -> {
-                val shareIntent = ShareCompat.IntentBuilder.from(activity)
-                    .setText(shareText)
-                    .setType("text/plain")
-                    .createChooserIntent()
-                    .apply {
-                        // https://android-developers.googleblog.com/2012/02/share-with-intents.html
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                            // If we're on Lollipop, we can open the intent as a document
-                            addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
-                        } else {
-                            // Else, we will use the old CLEAR_WHEN_TASK_RESET flag
-                            addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
-                        }
+    private fun createShareIntent() {
+        val shareIntent = ShareCompat.IntentBuilder.from(activity)
+                .setText(shareText)
+                .setType("text/plain")
+                .createChooserIntent()
+                .apply {
+                    // https://android-developers.googleblog.com/2012/02/share-with-intents.html
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        // If we're on Lollipop, we can open the intent as a document
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+                    } else {
+                        // Else, we will use the old CLEAR_WHEN_TASK_RESET flag
+                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
                     }
-                startActivity(shareIntent)
-                return true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+                }
+        startActivity(shareIntent)
     }
 
     // FloatingActionButtons anchored to AppBarLayouts have their visibility controlled by the scroll position.
