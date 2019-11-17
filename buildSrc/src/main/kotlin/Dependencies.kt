44539962bd7aa6e6b80e84f8a711b3a81071d119
Tom Hanley <tomas.hanley@toasttab.com>
2019-11-17 18:56:54 +0000

    Create buildSrc folder to apply kotlin-dsl and create dependencies file.

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+object Versions {
+    // Sdk and tools
+    const val compileSdkVersion = 28
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 28
+
+    // App dependencies
+    const val appCompatVersion = "1.1.0-beta01"
+    const val constraintLayoutVersion = "2.0.0-beta1"
+    const val coreTestingVersion = "2.0.0"
+    const val coroutinesVersion = "1.3.0-M2"
+    const val espressoVersion = "3.1.1"
+    const val fragmentVersion = "1.1.0-alpha09"
+    const val glideVersion = "4.9.0"
+    const val gradleVersion = "3.5.1"
+    const val gsonVersion = "2.8.2"
+    const val junitVersion = "4.12"
+    const val kotlinVersion = "1.3.41"
+    const val ktlintVersion = "0.33.0"
+    const val ktxVersion = "1.0.2"
+    const val lifecycleVersion = "2.2.0-alpha01"
+    const val materialVersion = "1.1.0-alpha09"
+    const val navigationVersion = "2.0.0"
+    const val recyclerViewVersion = "1.1.0-alpha05"
+    const val roomVersion = "2.1.0"
+    const val runnerVersion = "1.0.1"
+    const val truthVersion = "0.42"
+    const val testExtJunit = "1.1.0"
+    const val uiAutomatorVersion = "2.2.0"
+    const val viewPagerVersion = "1.0.0-rc01"
+    const val workVersion = "2.1.0"
+}
