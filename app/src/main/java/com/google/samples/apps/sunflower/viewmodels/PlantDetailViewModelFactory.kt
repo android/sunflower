9a345fea17222ee8c0955ae90f97a2a382ca5c5c
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-05-12 06:15:19 +0900

    Implement ViewModelProvider.Factory (#602)
    Co-authored-by: Lady Nicole <borrelli@google.com>

--- app/src/main/java/com/google/samples/apps/sunflower/viewmodels/PlantDetailViewModelFactory.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/viewmodels/PlantDetailViewModelFactory.kt
@@ -31,7 +31,7 @@ class PlantDetailViewModelFactory(
     private val plantRepository: PlantRepository,
     private val gardenPlantingRepository: GardenPlantingRepository,
     private val plantId: String
-) : ViewModelProvider.NewInstanceFactory() {
+) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
