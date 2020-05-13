4348adbf5479610940598bba98057ca01c8e2f2a
MR3Y <abdonasr379@gmail.com>
2020-05-13 21:57:01 +0200

    Replace ShareCompat with ShareSheet

--- app/src/main/java/com/google/samples/apps/sunflower/PlantDetailFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/PlantDetailFragment.kt
@@ -17,12 +17,12 @@
 package com.google.samples.apps.sunflower
 
 import android.content.Intent
+import android.content.Intent.*
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -31,7 +31,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.models.Plant
 import com.google.samples.apps.sunflower.databinding.FragmentPlantDetailBinding
 import com.google.samples.apps.sunflower.utilities.InjectorUtils
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -113,7 +113,6 @@ class PlantDetailFragment : Fragment() {
 
     // Helper function for calling a share functionality.
     // Should be used when user presses a share button/menu item.
-    @Suppress("DEPRECATION")
     private fun createShareIntent() {
         val shareText = plantDetailViewModel.plant.value.let { plant ->
             if (plant == null) {
@@ -122,12 +121,15 @@ class PlantDetailFragment : Fragment() {
                 getString(R.string.share_text_plant, plant.name)
             }
         }
-        val shareIntent = ShareCompat.IntentBuilder.from(activity)
-            .setText(shareText)
-            .setType("text/plain")
-            .createChooserIntent()
-            .addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
-        startActivity(shareIntent)
+
+        val shareIntent = Intent().apply {
+            action = ACTION_SEND
+            flags = FLAG_ACTIVITY_NEW_DOCUMENT or FLAG_ACTIVITY_MULTIPLE_TASK
+            type = "text/plain"
+            putExtra(EXTRA_TEXT, shareText)
+        }
+
+        startActivity(createChooser(shareIntent, null))
     }
 
     // FloatingActionButtons anchored to AppBarLayouts have their visibility controlled by the scroll position.
