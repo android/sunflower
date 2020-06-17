4d60f546618790aed2d92f9f52fc49a70141880b
Tiem Song <tiem@google.com>
2020-06-16 22:20:33 -0700

    Add the Unsplash Service

---
+++ app/src/main/java/com/google/samples/apps/sunflower/api/UnsplashService.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower.api
+
+import com.google.samples.apps.sunflower.BuildConfig
+import com.google.samples.apps.sunflower.data.UnsplashSearchResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface UnsplashService {
+
+    // TODO remove me
+    // Unsplash pagination docs: https://unsplash.com/documentation#pagination
+
+    @GET("search/photos")
+    suspend fun searchPhotos(
+        @Query("query") query: String,
+        @Query("client_id") clientId: String = BuildConfig.UNSPLASH_ACCESS_KEY,
+        @Query("page") page: Int = 1,
+        @Query("per_page") perPage: Int = 10
+    ) : UnsplashSearchResponse
+
+    companion object {
+        private const val BASE_URL = "https://api.unsplash.com/"
+
+        fun create(): UnsplashService {
+            val logger = HttpLoggingInterceptor().apply { level = Level.BASIC }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(UnsplashService::class.java)
+        }
+    }
+}
