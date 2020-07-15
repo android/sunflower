a4e9eace7c5737d4125d1b9db0abcfa79792fb91
Tiem Song <tiembo@users.noreply.github.com>
2020-07-15 10:52:26 -0700

    Add Paging 3.0 for gallery screen (#629)
    * Add Paging dependency
    
    Also updates RecyclerView version and update ShareCompat API call
    
    * Update UnsplashService param order
    
    Move client_id to the last param
    
    * Implement Paging 3.0
    
    Also remove default values for UnsplashService#searchPhotos
    
    * Remove unused method
    
    * Update paging version
    
    Co-authored-by: Florina Muntenescu <2998890+florina-muntenescu@users.noreply.github.com>
    
    * Update recyclerview version
    
    Co-authored-by: Florina Muntenescu <2998890+florina-muntenescu@users.noreply.github.com>
    
    * Address PR comments
    
    - catch Exception instead of IO/HttpException
    - disable Paging placeholders
    - remove unneeded local variable in searchPictures
    
    Co-authored-by: Florina Muntenescu <2998890+florina-muntenescu@users.noreply.github.com>

--- app/src/main/java/com/google/samples/apps/sunflower/api/UnsplashService.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/api/UnsplashService.kt
@@ -34,9 +34,9 @@ interface UnsplashService {
     @GET("search/photos")
     suspend fun searchPhotos(
         @Query("query") query: String,
-        @Query("client_id") clientId: String = BuildConfig.UNSPLASH_ACCESS_KEY,
-        @Query("page") page: Int = 1,
-        @Query("per_page") perPage: Int = 20
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int,
+        @Query("client_id") clientId: String = BuildConfig.UNSPLASH_ACCESS_KEY
     ) : UnsplashSearchResponse
 
     companion object {
