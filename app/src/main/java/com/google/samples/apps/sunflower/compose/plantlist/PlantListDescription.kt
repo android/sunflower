6af8a9f64a15e3af79c09f3c9593cbaa61b65af7
MR3Y <abdonasr379@gmail.com>
2020-09-23 02:04:10 +0200

    Add PlantListItem Composable
    the equivalent of list_item_plant.xml

--- app/src/main/java/com/google/samples/apps/sunflower/compose/plantlist/PlantListDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/compose/plantlist/PlantListDescription.kt
@@ -16,6 +16,7 @@
 
 package com.google.samples.apps.sunflower.compose.plantlist
 
+import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,7 +24,48 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.accessibilityLabel
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextAlign
 import com.google.samples.apps.sunflower.compose.Dimens
+import dev.chrisbanes.accompanist.coil.CoilImageWithCrossfade
+import com.google.samples.apps.sunflower.R
+
+@Composable
+fun PlantListItem(
+        plantName: String,
+        imgUrl: String,
+        onClick: () -> Unit,
+        modifier: Modifier = Modifier
+) {
+    CardView(onClick = onClick, modifier) {
+        //Also, we can use constraint layout instead of column here
+        Column(modifier = modifier.fillMaxWidth()
+                .wrapContentHeight()) {
+            val plantImgDesc = stringResource(R.string.a11y_plant_item_image)
+            CoilImageWithCrossfade(
+                    data = imgUrl,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.height(Dimens.PlantItemHeight)
+                            .fillMaxWidth()
+                            .semantics {
+                                accessibilityLabel = plantImgDesc
+                            }
+            )
+            Text(
+                    text = plantName,
+                    //Mimics ?attr/textAppearanceListItem textAppearance
+                    style = MaterialTheme.typography.subtitle1.copy(color = MaterialTheme.colors.onSurface),
+                    modifier = Modifier.padding(vertical = Dimens.MarginNormal)
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                    textAlign = TextAlign.Center
+            )
+        }
+    }
+}
 
 //Equivalent to MaskedCardView
 @Composable
