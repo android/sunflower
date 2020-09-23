bf1fad798992463b1d257a8f63528c1a0f5e3257
MR3Y <abdonasr379@gmail.com>
2020-09-23 02:39:48 +0200

    Add PreviewUtils for Previews
    Provide rapid visual feedback by adding some @Previews

---
+++ app/src/main/java/com/google/samples/apps/sunflower/compose/plantlist/FakeData.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower.compose.plantlist
+
+import com.google.samples.apps.sunflower.data.Plant
+
+/**
+ * Fake Data just for sake of preview Utils
+ */
+val plants = listOf(
+        Plant(
+                "malus-pumila",
+                "Apple",
+                "Fake Apple Description",
+                3,
+                30,
+                "https://upload.wikimedia.org/wikipedia/commons/5/55/Apple_orchard_in_Tasmania.jpg"),
+        Plant(
+                "beta-vulgaris",
+                "Beet",
+                "Fake beet description",
+                2,
+                7,
+                "https://upload.wikimedia.org/wikipedia/commons/2/29/Beetroot_jm26647.jpg"),
+        Plant(
+                "coriandrum-sativum",
+                "Cilantro",
+                "Fake Cilantro description",
+                2,
+                2,
+                "https://upload.wikimedia.org/wikipedia/commons/5/51/A_scene_of_Coriander_leaves.JPG"),
+        Plant(
+                "solanum-lycopersicum",
+                "Tomato",
+                "Fake Tomato description",
+                9,
+                4,
+                "https://upload.wikimedia.org/wikipedia/commons/1/17/Cherry_tomatoes_red_and_green_2009_16x9.jpg"),
+        Plant(
+                "persea-americana",
+                "Avocado",
+                "Fake Avocado Description",
+                9,
+                3,
+                "https://upload.wikimedia.org/wikipedia/commons/e/e4/Branch_and_fruit_of_the_Maluma_avocado_cultivar.jpg"),
+        Plant(
+                "pyrus-communis",
+                "Pear",
+                "Fake Pear description",
+                3,
+                30,
+                "https://upload.wikimedia.org/wikipedia/commons/1/13/More_pears.jpg"),
+        Plant(
+                "solanum-melongena",
+                "Eggplant",
+                "Fake Eggplant description",
+                4,
+                3,
+                "https://upload.wikimedia.org/wikipedia/commons/e/e5/Eggplant_display.JPG"),
+        Plant(
+                "vitis-vinifera",
+                "Grape",
+                "Fake Grape description",
+                9,
+                9,
+                "https://upload.wikimedia.org/wikipedia/commons/0/03/Grape_Plant_and_grapes9.jpg")
+        )
---
+++ app/src/main/java/com/google/samples/apps/sunflower/compose/plantlist/PreviewUtils.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower.compose.plantlist
+
+import androidx.compose.runtime.Composable
+import androidx.ui.tooling.preview.Preview
+import com.google.android.material.composethemeadapter.MdcTheme
+import com.google.samples.apps.sunflower.compose.ProvideDisplayInsets
+
+@Preview(showBackground = true, backgroundColor = 0xff49bb79)
+@Composable
+fun PreviewItem() {
+    PlantListItem(
+            plantName = "Tomato",
+            imgUrl = "https://upload.wikimedia.org/wikipedia/commons/1/17/Cherry_tomatoes_red_and_green_2009_16x9.jpg",
+            onClick = {})
+}
+
+@Preview(showBackground = true, backgroundColor = 0xff49bb79)
+@Composable
+fun PreviewPlantList() {
+    MdcTheme {
+        ProvideDisplayInsets {
+            PlantListScreen(plants = plants, onClick = {})
+        }
+    }
+}
