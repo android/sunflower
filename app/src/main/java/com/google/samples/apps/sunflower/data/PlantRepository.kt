dd276a610621f79fe3a6cb8bb1bb9711d670e427
Tiem Song <tiem@google.com>
2020-12-02 22:40:31 -0800

    Update coroutines to 1.4.0, remove unused import, add comment

--- app/src/main/java/com/google/samples/apps/sunflower/data/PlantRepository.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/data/PlantRepository.kt
@@ -16,12 +16,14 @@
 
 package com.google.samples.apps.sunflower.data
 
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 /**
  * Repository module for handling data operations.
+ *
+ * Collecting from the Flows in [PlantDao] is main-safe.  Room supports Coroutines and moves the
+ * query execution off of the main thread.
  */
 @Singleton
 class PlantRepository @Inject constructor(private val plantDao: PlantDao) {
