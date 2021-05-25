64431882eba6cb1e2e59ac10bdaf3e6f0c98ac93
manavbrar <manavbrar@gmail.com>
2021-05-24 20:31:45 -0700

    1) apdate versions on kotlin, appcompat, constraintlayout, coretesting, espresso, fragment, glide, gson, hilt, junit, ktx, lifecycle, material, navigation, paging, recycler, room, truth, testExt, and work 2) remove lifecycle-extensions in favour of lifecycle-runtime-ktx 3) remove deprecated import on observe 4) update gradle version 5) add progard for gson update 6) fix unimplemented method error

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -32,3 +32,15 @@
 -keepclassmembernames class kotlinx.** {
     volatile <fields>;
 }
+
+# Prevent proguard from stripping interface information from TypeAdapter, TypeAdapterFactory,
+# JsonSerializer, JsonDeserializer instances (so they can be used in @JsonAdapter)
+-keep class * implements com.google.gson.TypeAdapter
+-keep class * implements com.google.gson.TypeAdapterFactory
+-keep class * implements com.google.gson.JsonSerializer
+-keep class * implements com.google.gson.JsonDeserializer
+
+# Prevent R8 from leaving Data object members always null
+-keepclassmembers,allowobfuscation class * {
+  @com.google.gson.annotations.SerializedName <fields>;
+}
--- app/src/main/java/com/google/samples/apps/sunflower/data/UnsplashPagingSource.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/data/UnsplashPagingSource.kt
@@ -17,6 +17,7 @@
 package com.google.samples.apps.sunflower.data
 
 import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import com.google.samples.apps.sunflower.api.UnsplashService
 
 private const val UNSPLASH_STARTING_PAGE_INDEX = 1
@@ -39,5 +40,34 @@ class UnsplashPagingSource(
         } catch (exception: Exception) {
             LoadResult.Error(exception)
         }
+    }
+
+    /**
+     * Provide a [Key] used for the initial [load] for the next [PagingSource] due to invalidation
+     * of this [PagingSource]. The [Key] is provided to [load] via [LoadParams.key].
+     *
+     * The [Key] returned by this method should cause [load] to load enough items to
+     * fill the viewport around the last accessed position, allowing the next generation to
+     * transparently animate in. The last accessed position can be retrieved via
+     * [state.anchorPosition][PagingState.anchorPosition], which is typically
+     * the top-most or bottom-most item in the viewport due to access being triggered by binding
+     * items as they scroll into view.
+     *
+     * For example, if items are loaded based on integer position keys, you can return
+     * [state.anchorPosition][PagingState.anchorPosition].
+     *
+     * Alternately, if items contain a key used to load, get the key from the item in the page at
+     * index [state.anchorPosition][PagingState.anchorPosition].
+     *
+     * @param state [PagingState] of the currently fetched data, which includes the most recently
+     * accessed position in the list via [PagingState.anchorPosition].
+     *
+     * @return [Key] passed to [load] after invalidation used for initial load of the next
+     * generation. The [Key] returned by [getRefreshKey] should load pages centered around
+     * user's current viewport. If the correct [Key] cannot be determined, `null` can be returned
+     * to allow [load] decide what default key to use.
+     */
+    override fun getRefreshKey(state: PagingState<Int, UnsplashPhoto>): Int? {
+        TODO("Not yet implemented")
     }
 }
