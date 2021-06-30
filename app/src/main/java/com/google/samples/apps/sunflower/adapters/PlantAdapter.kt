4d4d0a3e725f12da548d0a1445e3c16a6a973623
Manuel Vivo <mvivo@google.com>
2021-06-30 07:32:02 +0000

    Migrates PlantAdapter to Compose

--- app/src/main/java/com/google/samples/apps/sunflower/adapters/PlantAdapter.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/adapters/PlantAdapter.kt
@@ -19,12 +19,34 @@ package com.google.samples.apps.sunflower.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.accompanist.glide.rememberGlidePainter
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.samples.apps.sunflower.HomeViewPagerFragmentDirections
 import com.google.samples.apps.sunflower.PlantListFragment
+import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.databinding.ListItemPlantBinding
 
@@ -34,27 +56,55 @@ import com.google.samples.apps.sunflower.databinding.ListItemPlantBinding
 class PlantAdapter : ListAdapter<Plant, RecyclerView.ViewHolder>(PlantDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return PlantViewHolder(
-            ListItemPlantBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
+        return PlantViewHolder(ComposeView(parent.context))
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val plant = getItem(position)
-        (holder as PlantViewHolder).bind(plant)
+        (holder as PlantViewHolder).bindViewHolder(plant)
+    }
+
+    override fun onViewRecycled(holder: RecyclerView.ViewHolder) {
+        if (holder is ComposeViewHolder<*>) {
+            holder.composeView.disposeComposition()
+        }
+        super.onViewRecycled(holder)
     }
 
     class PlantViewHolder(
-        private val binding: ListItemPlantBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
-        init {
-            binding.setClickListener {
-                binding.plant?.let { plant ->
-                    navigateToPlant(plant, it)
+        composeView: ComposeView
+    ) : ComposeViewHolder<Plant>(composeView) {
+
+        @OptIn(ExperimentalMaterialApi::class)
+        @Composable
+        override fun ViewHolder(input: Plant) {
+            MdcTheme {
+                Card(
+                    onClick = { navigateToPlant(input, composeView) },
+                    elevation = dimensionResource(id = R.dimen.card_elevation),
+                    shape = MaterialTheme.shapes.large,
+                    modifier = Modifier
+                        .padding(horizontal = dimensionResource(id = R.dimen.card_side_margin))
+                        .padding(bottom = dimensionResource(id = R.dimen.card_bottom_margin))
+                ) {
+                    Column(Modifier.fillMaxWidth()) {
+                        Image(
+                            painter = rememberGlidePainter(request = input.imageUrl, fadeIn = true),
+                            contentScale = ContentScale.Crop,
+                            contentDescription = stringResource(R.string.a11y_plant_item_image),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(dimensionResource(id = R.dimen.plant_item_image_height))
+                        )
+                        Text(
+                            text = input.name,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = dimensionResource(id = R.dimen.margin_normal))
+                                .wrapContentWidth(Alignment.CenterHorizontally)
+                        )
+                    }
                 }
             }
         }
@@ -68,13 +118,6 @@ class PlantAdapter : ListAdapter<Plant, RecyclerView.ViewHolder>(PlantDiffCallba
                     plant.plantId
                 )
             view.findNavController().navigate(direction)
-        }
-
-        fun bind(item: Plant) {
-            binding.apply {
-                plant = item
-                executePendingBindings()
-            }
         }
     }
 }
