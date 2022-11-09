7461fa37f0081e7bd82c1888b8ab765463f3b6f2
Chris Arriola <carriola@google.com>
2022-11-09 11:31:24 -0800

    Exclude version files in release mode.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,6 +100,14 @@ android {
   }
 }
 
+androidComponents {
+  onVariants(selector().withBuildType("release")) {
+    // Only exclude *.version files in release mode as debug mode requires
+    // these files for layout inspector to work.
+    it.packaging.resources.excludes.add("META-INF/*.version")
+  }
+}
+
 dependencies {
   kapt(libs.androidx.room.compiler)
   kapt(libs.hilt.android.compiler)
