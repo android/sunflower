5be6381a272156fbd3ccb3393cb28738725da931
Klejvi Kapaj <40796367+kl3jvi@users.noreply.github.com>
2022-11-22 23:22:25 +0100

    Changed TestCoroutineDispatcher (deprecated) to StandardTestDispatcher (#790)
    Co-authored-by: Chris Arriola <carriola@google.com>

--- app/src/androidTest/java/com/google/samples/apps/sunflower/MainCoroutineRule.kt
+++ app/src/androidTest/java/com/google/samples/apps/sunflower/MainCoroutineRule.kt
@@ -17,30 +17,25 @@
 package com.google.samples.apps.sunflower
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.*
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 class MainCoroutineRule(
-    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+    val testDispatcher: TestDispatcher = StandardTestDispatcher()
 ) : TestWatcher() {
 
-    override fun starting(description: Description?) {
+    override fun starting(description: Description) {
         super.starting(description)
         Dispatchers.setMain(testDispatcher)
     }
 
-    override fun finished(description: Description?) {
+    override fun finished(description: Description) {
         super.finished(description)
         Dispatchers.resetMain()
-        testDispatcher.cleanupTestCoroutines()
     }
 }
 
-fun MainCoroutineRule.runBlockingTest(block: suspend () -> Unit) =
-    this.testDispatcher.runBlockingTest {
-        block()
-    }
+fun MainCoroutineRule.runBlockingTest(block: suspend () -> Unit) = runTest(this.testDispatcher) {
+    block()
+}
