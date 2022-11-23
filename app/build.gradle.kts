713bc4640a789535615824af43b4c8d50c1b97f3
Chris Arriola <carriola@google.com>
2022-11-22 16:13:38 -0800

    Use 2022.10.00 BOM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -135,6 +135,7 @@ dependencies {
   implementation(libs.androidx.tracing.ktx)
 
   // Compose
+  implementation(platform(libs.androidx.compose.bom))
   implementation(libs.androidx.activity.compose)
   implementation(libs.androidx.constraintlayout.compose)
   implementation(libs.androidx.compose.runtime)
@@ -152,6 +153,7 @@ dependencies {
 
   // Testing dependencies
   kaptAndroidTest(libs.hilt.android.compiler)
+  androidTestImplementation(platform(libs.androidx.compose.bom))
   androidTestImplementation(libs.androidx.arch.core.testing)
   androidTestImplementation(libs.androidx.espresso.contrib)
   androidTestImplementation(libs.androidx.espresso.core)
