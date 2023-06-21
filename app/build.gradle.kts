e343b7feb32f4c9ae38aae63294dacf1c8546954
Qamar Safadi <qamar.elsafadi98@gmail.com>
2023-06-21 20:58:44 +0300

    feat: add material 3 dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -142,6 +142,7 @@ dependencies {
   implementation(libs.androidx.compose.foundation)
   implementation(libs.androidx.compose.foundation.layout)
   implementation(libs.androidx.compose.material)
+  implementation(libs.androidx.compose.material3)
   implementation(libs.androidx.compose.ui.viewbinding)
   implementation(libs.androidx.compose.ui.tooling.preview)
   implementation(libs.androidx.compose.runtime.livedata)
