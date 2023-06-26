572f0770beba278913d084f4afc57bf8a2f39434
Ahmed Madhoun <68607257+ahmed-madhoun1@users.noreply.github.com>
2023-06-26 20:00:26 +0300

    Remove deprecated android.lifecycle.observe method on PlantListFragment (#882)

--- app/src/main/java/com/google/samples/apps/sunflower/PlantListFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/PlantListFragment.kt
@@ -25,7 +25,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.observe
 import com.google.samples.apps.sunflower.adapters.PlantAdapter
 import com.google.samples.apps.sunflower.databinding.FragmentPlantListBinding
 import com.google.samples.apps.sunflower.viewmodels.PlantListViewModel
