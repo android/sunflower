4cc11e55c85f9bc21d48903f25bac12522aa27e5
borednaeem <ahmed.m.naeem@outlook.com>
2023-10-28 19:59:34 +0200

    - add plugins section to version catalog and update gradle build files (#918)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,17 +19,15 @@ buildscript {
         google()
         mavenCentral()
     }
-
-    dependencies {
-        classpath(libs.android.gradle.plugin)
-        classpath(libs.kotlin.gradle.plugin)
-        classpath(libs.hilt.android.gradle.plugin)
-    }
 }
 
 plugins {
-    id("com.diffplug.spotless") version "6.4.1"
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.android.test) apply false
 }
 
 spotless {
