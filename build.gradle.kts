cbab0831609e64b4ba5eb32100ce1fd282ac35f8
Chris Arriola <carriola@google.com>
2024-01-03 10:10:04 -0800

    Add workflow to update dependencies.
    Change-Id: Ic667c05d37eee5738bc24b2a18b2182093abb5c0

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,11 @@ plugins {
     alias(libs.plugins.spotless)
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.gradle.versions)
+    alias(libs.plugins.version.catalog.update)
 }
+
+apply("${project.rootDir}/buildscripts/toml-updater-config.gradle")
 
 spotless {
     kotlin {
